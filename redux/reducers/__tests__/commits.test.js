bd96c3e57d0b23b921e5a7f32bf6249cda7f7c5f
Joe Moody <jwmoody2@gmail.com>
2020-03-02 15:49:17 -0800

    [Joe] Going to expect a reducer that handles all of the state regarding our commits data that we will eventually get from Github.

---
+++ redux/reducers/__tests__/commits.test.js
@@ -0,0 +1,10 @@
+import commits from '../commits';
+import expect from 'expect';
+
+describe('commits reducer', () => {
+  it('Should return the initial state', () => {
+    expect(commits(undefined, {})).toEqual({
+
+    });
+  });
+});
