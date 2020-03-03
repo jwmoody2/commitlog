929dc46d1e728ddbe8974eae8799335767feb8dd
Joe Moody <jwmoody2@gmail.com>
2020-03-02 16:18:46 -0800

    [Joe] Created generic CommitList component

---
+++ components/commit-list/CommitList.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export class CommitList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <Text>CommitList!</Text>
+    );
+  }
+}
+
+export default CommitList;
