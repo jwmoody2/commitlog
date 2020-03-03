d8e0848b5cd3441fcff6838c9d36f35ceadbc853
Joe Moody <jwmoody2@gmail.com>
2020-03-02 17:49:57 -0800

    Added action to fetch commits from github and dispathc commitsReceieved

--- redux/actions/commits.tsx
+++ redux/actions/commits.tsx
@@ -1,4 +1,15 @@
+import axios from 'axios';
+
 export const commitsReceived = commits => ({
   commits,
   type: 'RECEIVE_COMMITS'
 });
+
+export function fetchCommits() {
+  return (dispatch) => {
+    return axios.get('https://api.github.com/repos/jwmoody2/commitlog/commits', {headers: {'Content-Type': 'application/json'}})
+      .then(response => {
+        dispatch(commitsReceived(response.data));
+      });
+  }
+}
