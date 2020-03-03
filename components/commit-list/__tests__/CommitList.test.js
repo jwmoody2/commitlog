b3899d5e9c3adad1402273ecc3681f2f7ecf35c3
Joe Moody <jwmoody2@gmail.com>
2020-03-02 16:22:12 -0800

    [Joe] I want to connect CommitList to the redux store to get the commits data

--- components/commit-list/__tests__/CommitList.test.js
+++ components/commit-list/__tests__/CommitList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import { CommitList } from '../CommitList';
 import Adapter from 'enzyme-adapter-react-16';
+import { idText } from 'typescript';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,4 +12,12 @@ describe('CommitList Component', () => {
 
     expect(wrapper.exists()).toBe(true);
   })
+
+  it('Values sent in as props should be mapped correctly', () => {
+    const initialState = {
+      commits: []
+    };
+
+    expect(mapStateToProps(initialState).commits).toEqual([]);
+  });
 });
