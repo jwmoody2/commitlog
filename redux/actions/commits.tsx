697029f8d7551eace035b5d7fab48de0d3cec37e
Joe Moody <jwmoody2@gmail.com>
2020-03-02 17:57:47 -0800

    Added a catch to fetchCommits

--- redux/actions/commits.tsx
+++ redux/actions/commits.tsx
@@ -5,11 +5,19 @@ export const commitsReceived = commits => ({
   type: 'RECEIVE_COMMITS'
 });
 
+export const commitsReceivedError = error => ({
+  error,
+  type: 'RECEIVE_COMMITS_ERROR'
+});
+
 export function fetchCommits() {
   return (dispatch) => {
     return axios.get('https://api.github.com/repos/jwmoody2/commitlog/commits', {headers: {'Content-Type': 'application/json'}})
       .then(response => {
         dispatch(commitsReceived(response.data));
+      })
+      .catch(error => {
+        dispatch(commitsReceivedError(error));
       });
   }
 }
